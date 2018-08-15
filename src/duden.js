e2dd32259f16c16985978aea57b25aa7f2837977
Feng Ya <fengya04@126.com>
2018-08-15 22:13:58 +0800

    load

--- src/duden.js
+++ src/duden.js
@@ -1,5 +1,7 @@
 const request = require('request')
 const cheerio = require('cheerio')
+const compact = require('lodash/compact')
+const flatten = require('lodash/flatten')
 
 const searchUrlBase = 'https://www.duden.de/suchen/dudenonline/'
 
@@ -16,4 +18,33 @@ function search (word) {
   })
 }
 
+function load (link) {
+  request(link, (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    const $ = cheerio.load(body)
+    const word = $('section#block-system-main > h1').text()
+    const cards = flatten(
+      $('section.term-section').toArray().map(section => {
+        const definition = section.parentNode.firstChild.data.trim()
+        return compact(
+          flatten(
+            section.children.map(child => {
+              switch (child.name) {
+                case 'span':
+                  return $(child).html()
+                case 'ul':
+                  return child.children.map(li => $(li).html())
+                default:
+              }
+            })
+          )
+        ).map(example => [example, `${word}<br>${definition}`])
+      })
+    )
+    console.log(cards)
+  })
+}
+
 search('Debatte')
+load('https://www.duden.de/rechtschreibung/Debatte')
