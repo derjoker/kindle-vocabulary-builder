a1ea33fa2a0599c0afb58e555eb9ebeb09b0167c
Feng Ya <fengya04@126.com>
2018-08-17 21:57:55 +0800

    frequency

--- src/duden.js
+++ src/duden.js
@@ -13,8 +13,12 @@ fs.writeFileSync(file, '')
 function search (word) {
   const url = searchUrlBase + word
   request(url, (error, response, body) => {
-    console.log('error:', error)
-    console.log('statusCode:', response && response.statusCode)
+    if (error || !response || !response.statusCode) {
+      console.log(word)
+      console.log('error:', error)
+      console.log('statusCode:', response && response.statusCode)
+      return
+    }
     const $ = cheerio.load(body)
     const links = $('#content section.wide > h2 > a')
       .toArray()
@@ -25,14 +29,22 @@ function search (word) {
 
 function load (link) {
   request(link, (error, response, body) => {
-    console.log(link)
-    console.log('error:', error)
-    console.log('statusCode:', response && response.statusCode)
-
-    if (error || !response || !response.statusCode) return
+    if (error || !response || !response.statusCode) {
+      console.log(link)
+      console.log('error:', error)
+      console.log('statusCode:', response && response.statusCode)
+      return
+    }
 
     const $ = cheerio.load(body)
     const word = $('section#block-system-main > h1').text()
+
+    const disabled = $('div > strong > span.disabled')
+    const frequency = disabled && 5 - disabled.text().split('').length
+
+    // console.log(frequency)
+    if (frequency < 3) return
+
     const cards = flatten(
       $('section.term-section').toArray().map(section => {
         const clone = $(section.parentNode).clone()
@@ -59,10 +71,10 @@ function load (link) {
       })
     )
     stringify(cards, (error, csv) => {
-      console.log('error:', error)
-      console.log(csv)
+      if (error) console.log('error:', error)
+      // console.log(csv)
       fs.writeFile(file, csv, { flag: 'a+' }, error => {
-        console.log('error:', error)
+        if (error) console.log('error:', error)
       })
     })
   })
@@ -70,4 +82,5 @@ function load (link) {
 
 // search('Debatte')
 // load('https://www.duden.de/rechtschreibung/Mund_Oeffnung_Lippen_Schlund')
+// load('https://www.duden.de/rechtschreibung/Aa_Kot')
 module.exports = search
