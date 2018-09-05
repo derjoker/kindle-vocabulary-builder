b43d6eed4c5d2a53f6385802a9c5419221322d34
Feng Ya <fengya04@126.com>
2018-09-05 23:33:39 +0800

    Word, Anki, List

--- server/src/model.js
+++ server/src/model.js
@@ -5,8 +5,17 @@ import Factory from './factory'
 
 const db = connect()
 
+const user = Schema({
+  _id: { type: String, alias: 'id' }, // hash(email)
+  email: { type: String, trim: true },
+  name: String
+})
+
+export const User = Factory(db, 'User', user)
+
 const vocab = Schema({
   _id: { type: String, alias: 'id' },
+  // userId: { type: String, alias: 'id' },
   usage: String,
   word: String,
   stem: String,
@@ -15,3 +24,47 @@ const vocab = Schema({
 })
 
 export const Vocab = Factory(db, 'Vocab', vocab)
+
+const word = Schema({
+  _id: { type: String, alias: 'id' }, // hash({link, word, example, definition})
+  link: String,
+  word: String,
+  example: String,
+  definition: String
+})
+
+export const Word = Factory(db, 'Word', word)
+
+// ankiId = wordId
+const anki = Schema({
+  _id: { type: String, alias: 'id' }, // wordId, TODO: hash({userId, wordId})
+  wordId: { type: String, alias: 'id' },
+  note: String,
+  category: String
+})
+
+export const Anki = Factory(db, 'Anki', anki)
+
+const list = Schema({
+  _id: { type: String, alias: 'id' },
+  name: String,
+  // userId: { type: String, alias: 'id' },
+  lang: String,
+  title: String,
+  stems: [String],
+  searches: [
+    {
+      stem: String,
+      links: [String]
+    }
+  ],
+  links: [
+    {
+      link: String,
+      wordIds: [String]
+    }
+  ],
+  ankiIds: [String]
+})
+
+export const List = Factory(db, 'List', list)
--- server/src/schema.js
+++ server/src/schema.js
@@ -3,6 +3,12 @@ import { gql } from 'apollo-server'
 import { Vocab } from './model'
 
 export const typeDefs = gql`
+type User {
+  id: ID!
+  email: String!
+  name: String!
+}
+
 type Vocab {
   id: ID!
   usage: String
@@ -13,12 +19,82 @@ type Vocab {
 }
 
 input VocabInput {
-  id: ID
+  id: ID!
+  # userId: ID
   usage: String
   word: String
   stem: String
   lang: String
   title: String
+}
+
+type Word {
+  id: ID!
+  link: String
+  word: String
+  example: String
+  definition: String
+}
+
+# Anki = Word + note + category
+type Anki {
+  id: ID!
+  link: String
+  word: String
+  example: String
+  definition: String
+  note: String
+  category: String
+}
+
+input AnkiInput {
+  id: ID!
+  wordId: ID!
+  note: String
+  category: String
+}
+
+type Search {
+  stem: String!
+  links: [String!]
+}
+
+input SearchInput {
+  stem: String!
+  links: [String!]
+}
+
+type Link {
+  link: String!
+  wordIds: [ID!]
+}
+
+input LinkInput {
+  link: String!
+  wordIds: [ID!]
+}
+
+type List {
+  id: ID!
+  name: String!
+  lang: String!
+  title: String
+  stems: [String]
+  searches: [Search]
+  links: [Link]
+  ankiIds: [ID!]
+}
+
+input ListInput {
+  id: ID!
+  name: String
+  userId: ID
+  lang: String
+  title: String
+  stems: [String]
+  searches: [SearchInput]
+  links: [LinkInput]
+  ankiIds: [ID!]
 }
 
 type Query {
