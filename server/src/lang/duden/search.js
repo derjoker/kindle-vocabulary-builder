131919441b09b86a84439e945637cfa0608157c6
Feng Ya <fengya04@126.com>
2018-09-11 07:08:11 +0800

    search word

--- server/src/lang/duden/search.js
+++ server/src/lang/duden/search.js
@@ -1,5 +1,7 @@
 export default function search ($) {
+  const word = $('#edit-q').val()
   return $('#content section.wide > h2 > a')
     .toArray()
+    .filter(link => $(link).text() === word)
     .map(link => link.attribs.href)
 }
--- server/src/lang/duden/search.test.js
+++ server/src/lang/duden/search.test.js
@@ -8,12 +8,18 @@ const path = resolve(__dirname, './data/suchen')
 
 describe('duden search', () => {
   it('runs', () => {
+    const length = {
+      Debatte: 1,
+      sein: 4
+    }
     const files = readdirSync(path).filter(file => file.endsWith('htm'))
     files.forEach(file => {
+      const word = file.replace('Duden _ Suchen _ ', '').replace('.htm', '')
       const html = readFileSync(resolve(path, file))
       const $ = load(html)
       const links = search($)
       expect(Array.isArray(links)).toBe(true)
+      expect(links.length).toBe(length[word])
     })
   })
 })
